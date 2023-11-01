3fc4d83ed37d161079959dcc02275358245bb6e1
shenny85 <143647445+shenny85@users.noreply.github.com>
2023-11-01 13:42:50 -0400

    added jsx app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//          5 REQUIRED STEPS
+// 1) import the React and ReactDOM libraries
+// 2) Get a reference to the div with ID root
+// 3) Tell React to take control of that element
+// 4) Create a component
+// 5) Show the component on the screen
+
+
+// 1) import the React and ReactDOM libraries
+import React from 'react';            //make components work together
+import ReactDOM from 'react-dom/client';   // get component to show up in the browser
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+
+// 4) Show the component on the screen
+root.render(<App />);
